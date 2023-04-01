d3b5044fa5dbadc51a4da7035d59c3f76d591365
Rahul Saha <rahul.saha.c@gmail.com>
2023-04-01 16:03:12 +0530

    refactor game_test

--- internal/domain/game_test.go
+++ internal/domain/game_test.go
@@ -2,7 +2,7 @@ package domain
 
 import "testing"
 
-func TestWinsShouldIncrementWhenPlayerWinsAMatch(t *testing.T) {
+func TestParikshitShouldHave1WinsWhenHeWinsAMatch(t *testing.T) {
 	parikshit := &Player{1, 0, 0}
 	rahul := &Player{2, 0, 0}
 	parikshit.WinAgainst(rahul)
@@ -11,11 +11,31 @@ func TestWinsShouldIncrementWhenPlayerWinsAMatch(t *testing.T) {
 	}
 }
 
-func TestLossesShouldIncrementWhenPlayerLosesAMatch(t *testing.T) {
+func TestParikshitShouldHave1LossWhenHeLosesAMatch(t *testing.T) {
 	parikshit := &Player{1, 0, 0}
 	rahul := &Player{2, 0, 0}
 	rahul.WinAgainst(parikshit)
 	if parikshit.losses != 1 {
 		t.Fatalf("Parikshit should have 1 loss")
+	}
+}
+
+func TestRahulShouldHave2WinsWhenHeWinsTwoMatches(t *testing.T) {
+	parikshit := &Player{1, 0, 0}
+	rahul := &Player{2, 0, 0}
+	rahul.WinAgainst(parikshit)
+	rahul.WinAgainst(parikshit)
+	if rahul.wins != 2 {
+		t.Fatalf("Rahul should have 2 wins")
+	}
+}
+
+func TestRahulShouldHave2LossessWhenHeLosesTwoMatches(t *testing.T) {
+	parikshit := &Player{1, 0, 0}
+	rahul := &Player{2, 0, 0}
+	parikshit.WinAgainst(rahul)
+	parikshit.WinAgainst(rahul)
+	if parikshit.wins != 2 {
+		t.Fatalf("Parikshit should have 2 wins")
 	}
 }
